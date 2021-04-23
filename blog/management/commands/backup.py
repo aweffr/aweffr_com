5520d114093bf0592c656bfd05df752855a74ba8
Zoule <zoule@eastmoney.com>
2021-04-23 14:31:03 +0800

    support send email after generate backup

--- blog/management/commands/backup.py
+++ blog/management/commands/backup.py
@@ -5,12 +5,19 @@ import subprocess
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from datetime import datetime
+from django.core.mail import send_mail, EmailMessage
+from argparse import ArgumentParser
 
 
 class Command(BaseCommand):
     help = "备份数据和media"
 
+    def add_arguments(self, parser: ArgumentParser):
+        parser.add_argument("--send-email", action='store_true')
+
     def handle(self, *args, **options):
+        send_email = options['send_email']
+
         cwd = settings.BASE_DIR
         dt = datetime.now().strftime('%Y-%m-%d')
         output_db = f"backup-{dt}.json"
@@ -23,3 +30,15 @@ class Command(BaseCommand):
             t.add(output_db, arcname=f"./backup-{dt}/" + output_db)
             t.add("./media/", arcname=f"./backup-{dt}/media/")
         os.remove(output_db)
+
+        if send_email:
+            msg = EmailMessage(
+                f"aweffr_com-备份-{dt}",
+                f"附件名: {output_tar}",
+                from_email="aweffr_dev@foxmail.com",
+                to=["aweffr@qq.com"],
+            )
+            with open(output_tar, 'rb') as f:
+                msg.attach(output_tar, f.read(), "application/octet-stream")
+
+            msg.send(fail_silently=False)
